74b061d1ce1df9258b5bf440fcc8cb7c6af16b43
Vivek <vivekyadav138001@gmail.com>
2025-04-27 17:38:20 +0530

    first commit

---
+++ src/main/java/com/PulsePoint/PulsePoint/PulsePointApplication.java
@@ -0,0 +1,13 @@
+package com.PulsePoint.PulsePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PulsePoint/PulsePoint/PulsePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PulsePoint.PulsePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
